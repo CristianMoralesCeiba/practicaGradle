32dd019b9f091ce286dbcb82478b1c24267930f5
CristianMoralesCeiba <cristian.morales@ceiba.com.co>
2017-11-07 15:53:55 -0500

    Configuración inicial
    Configuración inicial

---
+++ src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
@@ -0,0 +1,33 @@
+package co.ceiba.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import co.ceiba.domain.Person;
+import co.ceiba.testdatabuilder.PersonTestDataBuilder;
+
+public class NotifyPersonServiceTest {
+
+	private NotifyPersonService notifyPersonService;
+	private EmailService emailService;
+	
+	@Before
+	public void setUp() {
+		emailService = new EmailService();
+		notifyPersonService = new NotifyPersonService(emailService);
+	}
+	
+	@Test
+	public void test() {
+		//Arrage
+		Person person = new PersonTestDataBuilder().build();
+		//Act
+		String message = notifyPersonService.notify(person);
+		//Assert
+		Assert.assertNotNull(message);
+	}
+
+}
